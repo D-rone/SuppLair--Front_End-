02297a0f2eb44a660182c32f9d6e254652cbf87e
Mohamed Ouksili <medouksili@gmail.com>
2024-03-26 19:18:27 +0100

    color

--- src/components/home/HomeBody.jsx
+++ src/components/home/HomeBody.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 function HomeBody({ openSideBar }) {
   return (
     <div
-      className={`relative h-_pageBody bg-red-100 transition-all duration-300 ease-in-out ${
+      className={`relative h-_pageBody transition-all duration-300 ease-in-out ${
         openSideBar ? "w-4/5 left-1/5" : "w-full left-0"
       }`}
     >
