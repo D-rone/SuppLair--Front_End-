eaceb9fa64c780c89f709d70d0913a97bbd8e809
yassermessouter <105636024+yassermessouter@users.noreply.github.com>
2024-05-23 23:57:13 +0100

    Update Roles.jsx

--- src/components/home/users_roles/Roles.jsx
+++ src/components/home/users_roles/Roles.jsx
@@ -5,17 +5,32 @@ import dummyData from "./DUMMY_DATA.json";
 import { toast } from "react-toastify";
 import UpdateRolePopup from "../../pupups/UpdateRolePopup";
 import AddRolePopup from "../../pupups/AddRolePopup";
+import Cookies from "universal-cookie";
+import axios from "axios";
+import { useUserContext } from "../../../pages/HomePage";
 
 function Roles() {
+  const cookies = new Cookies();
+  const storedAccessToken = cookies.get("access_token");
+  const formData = new FormData();
+  const { userData, setUserData } = useUserContext();
   const [roles, setRoles] = useState([]);
+  const [role, setRole] = useState("");
 
   useEffect(() => {
-    getRoles();
+    axios
+      .get(`http://localhost:8080/api/v1/roles/` + userData.companyId, {
+        headers: {
+          Authorization: "Bearer " + storedAccessToken,
+        },
+      })
+      .then((res) => {
+        setRoles(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-
-  let getRoles = () => {
-    setRoles(dummyData.roles);
-  };
 
   const [updateRole, setUpdateRole] = useState(null);
 
@@ -49,20 +64,22 @@ function Roles() {
         <table className="w-[96%] mx-[2%]">
           <thead className="text-gray-400 border-b-2 border-gray-300">
             <tr>
-              <th className="px-[5%] py-4 font-normal w-[30%] text-start">Role Name</th>
+              <th className="px-[5%] py-4 font-normal w-[30%] text-start">
+                Role Name
+              </th>
               <th className="font-normal text-start">Access Rights</th>
               <th></th>
             </tr>
           </thead>
           <tbody className="text-lg">
             {roles.map((role) => (
-              <tr className={"border-b-2 border-gray-300 h-20"} key={v4()}> 
-                <td className="px-[4%] font-semibold hover:cursor-pointer text-supplair-primary">
-                  <h3 className="inline">{role.name}</h3>
+              <tr className={"border-b-2 border-gray-300 h-20"} key={v4()}>
+                <td className="px-[4%] font-medium hover:cursor-pointer text-supplair-primary">
+                  <h3 className="inline">{role.roleName}</h3>
                 </td>
-                <td className="relative font-semibold">
-                  {role.rights.join(", ")}
-                  {showDetails == role.id ? (
+                <td className="relative font-medium">
+                  {role.permissions.join(", ")}
+                  {showDetails == role.roleName ? (
                     <div className="absolute right-0 z-10 flex flex-col gap-2 p-2 bg-white rounded-lg shadow-sm -top-3 shadow-black h-fit w-fit">
                       <button
                         className="w-40 h-10 px-4 text-lg font-semibold rounded-lg hover:text-white hover:bg-supplair-primary text-start"
@@ -73,15 +90,6 @@ function Roles() {
                       >
                         Update
                       </button>
-                      <button
-                        className="w-40 h-10 px-4 text-lg font-semibold rounded-lg hover:text-white hover:bg-supplair-primary text-start"
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          toast.success("Role deleted");
-                        }}
-                      >
-                        Delete
-                      </button>
                     </div>
                   ) : (
                     <></>
@@ -91,7 +99,7 @@ function Roles() {
                   className="flex justify-center h-20 hover:cursor-pointer"
                   onClick={(e) => {
                     e.stopPropagation();
-                    setShowDetails(role.id);
+                    setShowDetails(role.roleName);
                   }}
                 >
                   <img src={showMore} alt="" className="w-6" />
@@ -100,7 +108,11 @@ function Roles() {
             ))}
           </tbody>
         </table>
-        {updateRole != null ? <UpdateRolePopup role={updateRole} close={setUpdateRole} /> : <></>}
+        {updateRole != null ? (
+          <UpdateRolePopup role={updateRole} close={setUpdateRole} />
+        ) : (
+          <></>
+        )}
         {addRole ? <AddRolePopup close={setAddRole} /> : <></>}
       </div>
     </div>
