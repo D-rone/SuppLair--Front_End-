930219db2628f6af13c78e26c0278c386d119f6c
yassermessouter <105636024+yassermessouter@users.noreply.github.com>
2024-06-09 14:38:28 +0100

    Delete SuppLair--Front_End--main directory

--- SuppLair--Front_End--main/src/components/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NotFound() {
-  return <div className="text-red-700 font-bold">404 Not Found</div>;
-}
-
-export default NotFound;
--- SuppLair--Front_End--main/src/components/super-admin/users/Users.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SuperAdminUsers() {
-  return (
-    <div>Super Admin Users</div>
-  )
-}
-
-export default SuperAdminUsers
